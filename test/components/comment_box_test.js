beffb57b9984c88fd9f8f63b411bec0542b47bb9
Amit Ranan <ranan.amit@gmail.com>
2017-01-11 15:53:40 -0800

    midway through CommentBox test

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -1,2 +1,14 @@
 import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/CommentBox';
+
+// we want to make sure CommentBox has a textarea, button, and the textarea updates as user is typing
+describe('CommentBox', () => {
+  it('has a text area', () => {
+    const component = renderComponent(CommentBox);
+    expect(component.find('textarea')).to.exist;
+  });
+
+  it('has a button', () => {
+
+  });
+});
