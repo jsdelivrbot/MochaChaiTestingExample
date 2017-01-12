55b790522dfb67fed2f7e6d030fac7156f0c97a4
Amit Ranan <ranan.amit@gmail.com>
2017-01-11 16:01:12 -0800

    add class test to CommentBox

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,6 +3,11 @@ import CommentBox from '../../src/components/CommentBox';
 
 // we want to make sure CommentBox has a textarea, button, and the textarea updates as user is typing
 describe('CommentBox', () => {
+  it('has the class "comment-box"', () => {
+    const component = renderComponent(CommentBox);
+    expect(component).to.have.class('comment-box');
+  })
+
   it('has a text area', () => {
     const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
